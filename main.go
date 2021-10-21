2076076ca47d9c17fb07ad6d32dd9448c458efe2
Alexander A. Klimov <alexander.klimov@icinga.com>
2021-10-21 12:46:16 +0200

    -w COMMAND: watch redis-cli COMMAND

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"github.com/gomodule/redigo/redis"
 	log "github.com/sirupsen/logrus"
+	"reflect"
 	"regexp"
+	"strings"
 	"sync"
 )
 
@@ -30,11 +32,44 @@ func (rf *rgxFlag) Set(s string) (err error) {
 func main() {
 	m := &rgxFlag{}
 	r := flag.String("r", "127.0.0.1:6379", "HOST:PORT")
+	w := flag.String("w", "", "REDIS COMMAND")
 	wg := &sync.WaitGroup{}
 
 	flag.Var(m, "m", "REGEX")
 	flag.Parse()
 	log.SetLevel(log.TraceLevel)
+
+	if *w != "" {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			cmd := strings.Split(*w, " ")
+			var prev interface{} = struct{}{}
+
+			args := make([]interface{}, 0, len(cmd)-1)
+			for _, arg := range cmd[1:] {
+				args = append(args, arg)
+			}
+
+			cn, err := redis.Dial("tcp", *r)
+			if err != nil {
+				panic(err)
+			}
+
+			for {
+				res, err := cn.Do(cmd[0], args...)
+				if err != nil {
+					panic(err)
+				}
+
+				if !reflect.DeepEqual(res, prev) {
+					prev = res
+					log.Info(res)
+				}
+			}
+		}()
+	}
 
 	if m.rgx != nil {
 		wg.Add(1)
